bcf266ce67017175786a47a101c5366649f4ae81
bill.cotton <bill.cotton@c0e136cc-504f-0410-aea0-bafa65729717>
2009-03-11 14:41:59 +0000

    New utility for sorting uv data

---
+++ ObitSystem/Obit/include/ObitUVSortBufferClassDef.h
@@ -0,0 +1,40 @@
+/* $Id:  $  */
+/*--------------------------------------------------------------------*/
+/*;  Copyright (C) 2009                                               */
+/*;  Associated Universities, Inc. Washington DC, USA.                */
+/*;  This program is free software; you can redistribute it and/or    */
+/*;  modify it under the terms of the GNU General Public License as   */
+/*;  published by the Free Software Foundation; either version 2 of   */
+/*;  the License, or (at your option) any later version.              */
+/*;                                                                   */
+/*;  This program is distributed in the hope that it will be useful,  */
+/*;  but WITHOUT ANY WARRANTY; without even the implied warranty of   */
+/*;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    */
+/*;  GNU General Public License for more details.                     */
+/*;                                                                   */
+/*;  You should have received a copy of the GNU General Public        */
+/*;  License along with this program; if not, write to the Free       */
+/*;  Software Foundation, Inc., 675 Massachusetts Ave, Cambridge,     */
+/*;  MA 02139, USA.                                                   */
+/*;                                                                   */
+/*;Correspondence about this software should be addressed as follows: */
+/*;         Internet email: bcotton@nrao.edu.                         */
+/*;         Postal address: William Cotton                            */
+/*;                         National Radio Astronomy Observatory      */
+/*;                         520 Edgemont Road                         */
+/*;                         Charlottesville, VA 22903-2475 USA        */
+/*--------------------------------------------------------------------*/
+/*  Define the basic components of the ObitUVSortBuffer ClassInfo structure */
+/* This is intended to be included in a classInfo structure definition  */
+/* and to be used as the template for generating new classes derived    */
+/* from Obit.                                                           */
+#include "ObitClassDef.h"  /* Parent class ClassInfo definition file */
+/** Function pointer to Constructor. */
+ObitUVSortBufferCreateFP ObitUVSortBufferCreate;
+/** Function pointer to AddVis. */
+ObitUVSortBufferAddVisFP ObitUVSortBufferAddVis;
+/** Function pointer to Sort. */
+ObitUVSortBufferSortFP ObitUVSortBufferSort;
+/** Function pointer to Flush. */
+ObitUVSortBufferFlushFP ObitUVSortBufferFlush;
+
---
+++ ObitSystem/Obit/include/ObitUVSortBufferDef.h
@@ -0,0 +1,49 @@
+/* $Id:  $ */
+/*--------------------------------------------------------------------*/
+/*;  Copyright (C) 2009                                               */
+/*;  Associated Universities, Inc. Washington DC, USA.                */
+/*;  This program is free software; you can redistribute it and/or    */
+/*;  modify it under the terms of the GNU General Public License as   */
+/*;  published by the Free Software Foundation; either version 2 of   */
+/*;  the License, or (at your option) any later version.              */
+/*;                                                                   */
+/*;  This program is distributed in the hope that it will be useful,  */
+/*;  but WITHOUT ANY WARRANTY; without even the implied warranty of   */
+/*;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    */
+/*;  GNU General Public License for more details.                     */
+/*;                                                                   */
+/*;  You should have received a copy of the GNU General Public        */
+/*;  License along with this program; if not, write to the Free       */
+/*;  Software Foundation, Inc., 675 Massachusetts Ave, Cambridge,     */
+/*;  MA 02139, USA.                                                   */
+/*;                                                                   */
+/*;Correspondence about this software should be addressed as follows: */
+/*;         Internet email: bcotton@nrao.edu.                         */
+/*;         Postal address: William Cotton                            */
+/*;                         National Radio Astronomy Observatory      */
+/*;                         520 Edgemont Road                         */
+/*;                         Charlottesville, VA 22903-2475 USA        */
+/*--------------------------------------------------------------------*/
+/*  Define the basic components of the ObitUVSortBuffer structure     */
+/*  This is intended to be included in a class structure definition   */
+/* and to be used as the template for generating new classes derived  */
+/* from Obit.                                                         */
+/**
+ * \file ObitUVSortBufferDef.h
+ * ObitUVSortBuffer structure members for this and any derived classes.
+ */
+#include "ObitDef.h"  /* Parent class instance definitions */
+/** Threading info member object  */
+ObitThread *thread;
+/** Linked list of arrays of data.  */
+ObitInfoList *info;
+/** ObitUV object */
+ObitUV *myUVdata;
+/** Buffer */
+ofloat *myBuffer;
+/** Sort structure array */
+ofloat *SortStruct;
+/** Number of visibilities */
+olong nvis;
+/** Highest number populated */
+olong hiVis;
